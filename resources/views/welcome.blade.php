c0f314053a44dfccf65eebebaeff81efa24bd875
alabadohermgild-sys <alabadohermgild.com>
2026-03-03 03:15:23 +0800

    final update with video fyyyix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         <div class="flex items-center space-x-6">
             <a href="#projects" class="hover:text-blue-600 transition hidden md:block">Projects</a>
             
-            <a href="https://www.facebook.com/ha.ngin.3990" target="_blank" class="text-gray-400 hover:text-blue-500 transition">
+            <a href="https://www.facebook.com/Slimshady477" target="_blank" class="text-gray-400 hover:text-blue-500 transition">
                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
             </a>
 
