4ffb7bea9aa723089011e4a849d618cd0869c6bc
alabadohermgild-sys <alabadohermgild.com>
2026-03-03 22:31:22 +0800

    registered weather route from root

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// THIS PART IS CRITICAL
+Route::get('/weather', function () {
+    return view('weather');
+});
